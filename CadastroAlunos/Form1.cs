ce6f50e765b55a3121f5005c83adf29426ef0c85
Matheus <matheusfffcorrea@gmail.com>
2025-04-03 11:02:57 -0300

    comitao

--- CadastroAlunos/Form1.cs
+++ CadastroAlunos/Form1.cs
@@ -12,6 +12,7 @@ namespace CadastroAlunos
             InitializeComponent();
         }
 
+
         private void groupBox2_Enter(object sender, EventArgs e) { }
 
 
@@ -86,18 +87,116 @@ namespace CadastroAlunos
             GrindView.DataSource = dataTable;
             GrindView.AutoResizeColumns();
 
-            conexao.Open();
+            conexao.Close();
         }
 
         private void btnEditar_Click(object sender, EventArgs e)
         {
+            try
+            {
 
+                string nomeAluno = txtNome.Text;
+                string Curso = txtCurso.Text;
+                string dataNascimento = mskdDataNascimento.Text;
+                string Telefone = mskdTelefone.Text;
+                int idAlunoSelected = Convert.ToInt32(GrindView.CurrentRow.Cells[0].Value.ToString());
+
+                DateTime dataMySqlFormat = Convert.ToDateTime(dataNascimento);
+                string dataNascimentoFormat = dataMySqlFormat.ToString("yyyy-MM-dd");
+
+                string conexaoBanco = "Server=localHost; Database=cadastroalunos; Uid=root; pwd=''";
+                MySqlConnection conexao = new MySqlConnection(conexaoBanco);
+
+                conexao.Open();
+
+
+
+                string update = "UPDATE cadastroalunos SET Nome = @nome, data_nascimento = @dataNascimento, Curso = @Curso, Telefone = @Telefone WHERE id = @id";
+                MySqlCommand updateAluno = new MySqlCommand(update, conexao);
+
+
+                updateAluno.Parameters.AddWithValue("@Nome", nomeAluno);
+                updateAluno.Parameters.AddWithValue("@dataNascimento", dataNascimentoFormat);
+                updateAluno.Parameters.AddWithValue("@Curso", Curso);
+                updateAluno.Parameters.AddWithValue("@Telefone", Telefone);
+                updateAluno.Parameters.AddWithValue("@id", idAlunoSelected);
+                    
+                int contLinhas = updateAluno.ExecuteNonQuery();
+
+
+
+                if (contLinhas > 0)
+                {
+                    MessageBox.Show("Dados atualizados!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Não foi possível editar!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                conexao.Close();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
         }
 
 
         private void btnExcluir_Click(object sender, EventArgs e)
         {
+            try
+            {
 
+                string nomeAluno = txtNome.Text;
+                string Curso = txtCurso.Text;
+                string dataNascimento = mskdDataNascimento.Text;
+                string Telefone = mskdTelefone.Text;
+
+                DateTime dataMySqlFormat = Convert.ToDateTime(dataNascimento);
+                string dataNascimentoFormat = dataMySqlFormat.ToString("yyyy-MM-dd");
+
+                string conexaoBanco = "Server=localHost; Database=cadastroalunos; Uid=root; pwd=''";
+                MySqlConnection conexao = new MySqlConnection(conexaoBanco);
+
+                conexao.Open();
+
+                string insert = "INSERT INTO cadastroalunos (Nome, data_nascimento, Curso, Telefone) values (@Nome, @dataNascimento, @Curso, @Telefone)";
+                MySqlCommand insertAluno = new MySqlCommand(insert, conexao);
+
+
+                insertAluno.Parameters.AddWithValue("@Nome", nomeAluno);
+                insertAluno.Parameters.AddWithValue("@dataNascimento", dataNascimentoFormat);
+                insertAluno.Parameters.AddWithValue("@Curso", Curso);
+                insertAluno.Parameters.AddWithValue("@Telefone", Telefone);
+
+                int contLinhas = Convert.ToInt32(insertAluno.ExecuteNonQuery());
+
+
+
+                if (contLinhas > 0)
+                {
+                    MessageBox.Show("Usuário cadastrado!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Não foi possível cadastrar!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                conexao.Close();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
